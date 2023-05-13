899b33a6a46eadeb84706a4aa471ff82cd501910
RansuAndorei <lancejuat26@gmail.com>
2023-05-13 13:03:15 +0800

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { MantineProvider } from "@mantine/core";
+import { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "light",
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
